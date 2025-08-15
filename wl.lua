5b2d3e8589d3b1d2372ec3620090666322f7e73b
aabbaaii13 <172551149+CaseohCASEOH@users.noreply.github.com>
2025-08-16 00:13:36 +0500

    Update wl.lua

--- wl.lua
+++ wl.lua
@@ -32,7 +32,7 @@ run = false
 local f,d="wl.key",(gethwid():gsub(".",function(c)return("%02x"):format(c:byte())end))
 if not isfile(f) then writefile(f,d)
 elseif readfile(f)~=d then game.Players.LocalPlayer:Kick("Error 243") writefile(f,d) end
-if not({["65373932636264616534386639636138373637323034346638633434336434656435646533623835316430313935303032616235643133656466353434396366"]=true})[readfile(f)] then game.Players.LocalPlayer:Kick("Error 233") end
+if not({["65373932636264616534386639636138373637323034346638633434336434656435646533623835316430313935303032616235643133656466353434396366"]=true}, {["36663130366239303934366437666364"]=true})[readfile(f)] then game.Players.LocalPlayer:Kick("Error 233") end
 local wl = { [f] = true }
 
 if not wl[f] then
